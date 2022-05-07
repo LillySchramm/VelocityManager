90d8e038245b3f6214cc9bbab9337ca0f6219d76
Elias Schramm <privat@eps-dev.de>
2022-05-07 18:30:26 +0200

    feat(velocity): Basic Server Initialization

--- velocity/src/main/java/de/epsdev/velocitymanager/Velocitymanager.java
+++ velocity/src/main/java/de/epsdev/velocitymanager/Velocitymanager.java
@@ -4,7 +4,12 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import de.epsdev.velocitymanager.lib.ServerType;
+import de.epsdev.velocitymanager.lib.VelocityServerManager;
+import de.epsdev.velocitymanager.lib.config.IConfig;
 import org.slf4j.Logger;
+
+import java.util.UUID;
 
 @Plugin(
         id = "velocitymanager",
@@ -14,12 +19,34 @@ import org.slf4j.Logger;
         authors = {"EliasSchramm"}
 )
 public class Velocitymanager {
-    TestClass testClass = new TestClass();
+    public VelocityServerManager serverManager;
+    private IConfig iConfig = new IConfig() {
+        @Override
+        public String getServerUUID() {
+            return "";
+        }
+
+        @Override
+        public void setServerUUID(UUID uuid) {
+
+        }
+
+        @Override
+        public String getToken() {
+            return "YWRtaW46YWRtaW4=";
+        }
+
+        @Override
+        public String getAPIUrl() {
+            return "http://192.168.2.100:30001/";
+        }
+    };
 
     @Inject
     private Logger logger;
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        this.serverManager = new VelocityServerManager(ServerType.PROXY_SERVER, iConfig);
     }
 }
