d8bf856b36a02cbe834362e1bd2e445431794843
Elias Schramm <privat@eps-dev.de>
2022-05-07 21:40:55 +0200

    feat: Basic Game Server Implementation

--- spigot/src/main/java/de/epsdev/velocitymanager/spigot/VelocityManager.java
+++ spigot/src/main/java/de/epsdev/velocitymanager/spigot/VelocityManager.java
@@ -1,13 +1,39 @@
 package de.epsdev.velocitymanager.spigot;
 
+import de.epsdev.velocitymanager.lib.ServerType;
+import de.epsdev.velocitymanager.lib.VelocityServerManager;
+import de.epsdev.velocitymanager.lib.config.IConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.UUID;
+
 public final class VelocityManager extends JavaPlugin {
-    TestClass testClass = new TestClass();
+    public VelocityServerManager serverManager;
+    private IConfig iConfig = new IConfig() {
+        @Override
+        public String getServerUUID() {
+            return "";
+        }
+
+        @Override
+        public void setServerUUID(UUID uuid) {
+
+        }
+
+        @Override
+        public String getToken() {
+            return "YWRtaW46YWRtaW4=";
+        }
+
+        @Override
+        public String getAPIUrl() {
+            return "http://192.168.2.100:30001/";
+        }
+    };
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        this.serverManager = new VelocityServerManager(ServerType.GAME_SERVER, iConfig);
     }
 
     @Override
