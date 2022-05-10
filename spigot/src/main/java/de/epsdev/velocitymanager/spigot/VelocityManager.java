35b86ca380bd2bf9bdbc583c51b1fa31f8c1bddb
Elias Schramm <privat@eps-dev.de>
2022-05-10 18:26:23 +0200

    feat(spigot): Implement Configuration

--- spigot/src/main/java/de/epsdev/velocitymanager/spigot/VelocityManager.java
+++ spigot/src/main/java/de/epsdev/velocitymanager/spigot/VelocityManager.java
@@ -3,7 +3,7 @@ package de.epsdev.velocitymanager.spigot;
 import de.epsdev.velocitymanager.lib.ServerType;
 import de.epsdev.velocitymanager.lib.VelocityServerManager;
 import de.epsdev.velocitymanager.lib.config.IConfig;
-import de.epsdev.velocitymanager.lib.config.ILogger;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.UUID;
@@ -11,30 +11,35 @@ import java.util.logging.Level;
 
 public final class VelocityManager extends JavaPlugin {
     public VelocityServerManager serverManager;
-    private IConfig iConfig = new IConfig() {
+    private FileConfiguration fileConfiguration;
+
+    private final IConfig iConfig = new IConfig() {
         @Override
         public String getServerUUID() {
-            return "";
+            return fileConfiguration.getString("uuid");
         }
 
         @Override
         public void setServerUUID(UUID uuid) {
-
+            fileConfiguration.set("uuid", uuid.toString());
+            saveConfig();
         }
 
         @Override
         public String getToken() {
-            return "YWRtaW46YWRtaW4=";
+            return fileConfiguration.getString("token");
         }
 
         @Override
         public String getAPIUrl() {
-            return "http://192.168.2.100:30001/";
+            return fileConfiguration.getString("api_url");
         }
     };
 
     @Override
     public void onEnable() {
+        initializeConfig();
+
         this.serverManager = new VelocityServerManager(
                 ServerType.GAME_SERVER,
                 iConfig,
@@ -42,8 +47,17 @@ public final class VelocityManager extends JavaPlugin {
         );
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    private void initializeConfig() {
+        fileConfiguration = getConfig();
+
+        fileConfiguration.addDefault("api_url", "");
+        fileConfiguration.addDefault("token", "");
+        fileConfiguration.addDefault("uuid", "");
+
+        fileConfiguration.options().copyDefaults(true);
+        saveConfig();
     }
+
+    @Override
+    public void onDisable() {}
 }
