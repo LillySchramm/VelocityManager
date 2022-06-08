e6e698dead242cacf8bea95f2fac99497ac4ddff
Elias Schramm <privat@eps-dev.de>
2022-06-08 20:19:39 +0200

    feat(api): Add Cors

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import basicAuth from 'express-basic-auth';
+import cors from 'cors';
 
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(cors());
 app.use(
     basicAuth({
         users: { admin: 'admin' },
