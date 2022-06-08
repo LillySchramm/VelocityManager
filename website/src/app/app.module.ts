3da338621e6e59f1e3af65c39f9256c72004afbe
Elias Schramm <privat@eps-dev.de>
2022-06-08 21:29:08 +0200

    feat(website): Add Login

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,21 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MenubarModule } from 'primeng/menubar';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { CheckboxModule } from 'primeng/checkbox';
+import {InputTextModule} from 'primeng/inputtext';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import {MessagesModule} from 'primeng/messages'
+
+import { LoginComponent } from './components/pages/login/login.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, LoginComponent, HomeComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         MenubarModule,
         TabMenuModule,
+        CheckboxModule,
+        InputTextModule,
+        HttpClientModule,
+        FormsModule,
+        CardModule,
+        ButtonModule,
+        ReactiveFormsModule,
+        ToastModule,
+        MessagesModule
     ],
     providers: [],
     bootstrap: [AppComponent],
---
+++ website/src/app/services/auth.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { forkJoin, fromEvent, Observable, of, Subject, throwError } from 'rxjs';
+import { take, catchError, combineAll, map } from 'rxjs/operators';
+import { PingResponse } from '../models/httpResponses.models';
+import { environment } from './../../environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    public isLoggedIn(): Observable<boolean> {
+        const credentials = this.getSavedCredentials();
+        if (!credentials) {
+            return of(false);
+        }
+
+        return this.http
+            .get<PingResponse>(environment.apiUrl + '/ping', {
+                headers: this.generateHeader(credentials),
+            })
+            .pipe(
+                map(() => {
+                    return true;
+                }),
+                catchError(() => {
+                    localStorage.removeItem('credentials')
+                    return of(false);
+                })
+            );
+    }
+
+    private generateHeader(credentials: String): { [header: string]: string } {
+        return { Authorization: 'Basic ' + credentials };
+    }
+
+    private getSavedCredentials(): String | null {
+        return localStorage.getItem('credentials');
+    }
+
+    public login(username: String, password: String): Observable<boolean> {
+        const credentials = btoa(`${username}:${password}`)
+
+        return this.http
+            .get<PingResponse>(environment.apiUrl + '/ping', {
+                headers: this.generateHeader(credentials),
+            })
+            .pipe(
+                map(() => {
+                    localStorage.setItem('credentials', credentials)
+                    return true;
+                }),
+                catchError(() => {
+                    return of(false);
+                })
+            );
+    }
+}
