e31cb780f8072a3cd1331ffec91a8c7ecc0005a0
Elias Schramm <privat@eps-dev.de>
2022-06-11 20:45:31 +0000

    refactor(fe): Use NGRX for State Management

--- website/src/app/components/pages/login/login.component.ts
+++ website/src/app/components/pages/login/login.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MessageService } from 'primeng/api';
-import { take, tap } from 'rxjs/operators';
-import { AuthService } from 'src/app/services/auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { login } from 'src/app/store/auth/auth.actions';
+import { selectAuthToken } from 'src/app/store/auth/auth.selectors';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
-    providers: [MessageService],
 })
 export class LoginComponent implements OnInit {
     formGroup = new FormGroup({
@@ -17,40 +16,22 @@ export class LoginComponent implements OnInit {
         password: new FormControl(''),
     });
 
-    constructor(
-        private authService: AuthService,
-        private router: Router,
-        private messageService: MessageService
-    ) {}
+    constructor(private router: Router, private store: Store) {}
 
     ngOnInit(): void {
-        this.authService
-            .isLoggedIn()
-            .pipe(take(1))
-            .subscribe((isLoggedIn) => {
-                if (isLoggedIn) {
-                    this.router.navigate(['home']);
-                }
-            });
+        this.store.select(selectAuthToken).subscribe((token) => {
+            if (token) {
+                this.router.navigate(['home']);
+            }
+        });
     }
 
     login(): void {
-        this.authService
-            .login(
-                this.formGroup.get('username')?.value,
-                this.formGroup.get('password')?.value
-            )
-            .pipe(take(1))
-            .subscribe((result) => {
-                if (result) {
-                    this.router.navigate(['home']);
-                    return;
-                }
-                this.messageService.add({
-                    severity: 'error',
-                    summary: 'Login failed!',
-                    detail: 'Please check username and password',
-                });
-            });
+        this.store.dispatch(
+            login({
+                username: this.formGroup.get('username')?.value,
+                password: this.formGroup.get('password')?.value,
+            })
+        );
     }
 }
---
+++ website/src/app/store/auth/auth.effects.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
+import {
+    catchError,
+    exhaustMap,
+    map,
+    mergeMap,
+    switchMap,
+    tap,
+} from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
+import { addMessagesToQueue } from '../message/message.actions';
+import {
+    loadCredentials,
+    loadCredentialsFail,
+    loadCredentialsSuccess,
+    login,
+    loginFail,
+    loginSuccess,
+    logout,
+    logoutSuccess,
+} from './auth.actions';
+
+@Injectable()
+export class AuthEffects {
+    loadCredentials$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(loadCredentials),
+            map(() => this.authService.getSavedCredentials()),
+            exhaustMap((token) =>
+                this.authService.verifyCredentials(token).pipe(
+                    map((valid) => {
+                        return valid ? token : null;
+                    })
+                )
+            ),
+            map((token) => {
+                if (!token) {
+                    return loadCredentialsFail();
+                }
+
+                return loadCredentialsSuccess({ token });
+            })
+        )
+    );
+    login$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(login),
+            switchMap((payload) =>
+                this.authService.login(payload.username, payload.password)
+            ),
+            map((token) => {
+                if (!token) {
+                    this.store.dispatch(
+                        addMessagesToQueue({
+                            messages: [
+                                {
+                                    severity: 'error',
+                                    summary: 'Login failed!',
+                                    detail: 'Please check username and password',
+                                },
+                            ],
+                        })
+                    );
+                    return loginFail();
+                }
+
+                this.store.dispatch(
+                    addMessagesToQueue({
+                        messages: [
+                            {
+                                severity: 'success',
+                                summary: 'Login successful!',
+                            },
+                        ],
+                    })
+                );
+
+                return loginSuccess({ token });
+            })
+        )
+    );
+
+    logout$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(logout),
+            map(() => {
+                this.authService.logout();
+
+                this.store.dispatch(
+                    addMessagesToQueue({
+                        messages: [
+                            {
+                                severity: 'info',
+                                summary: 'Logout successful!',
+                            },
+                        ],
+                    })
+                );
+
+                return logoutSuccess();
+            })
+        )
+    );
+    constructor(
+        private actions$: Actions,
+        private authService: AuthService,
+        private store: Store
+    ) {}
+}
