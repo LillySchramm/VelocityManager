3e901b9c56383ee701be6cf83f110d432693266f
Elias Schramm <privat@eps-dev.de>
2022-09-23 00:51:23 +0200

    dev: Add docker-compose for faster dev setup

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Run <br> `npx prettier --write .`
 ## Javadocs
 
 https://velocitymanager.docs.eps-dev.de/
+
+## Development
+
+To start the most basic environment:
+`docker-compose up`
+
+To start a environment with multiple Minecraft servers:
+`docker-compose up --scale paper=2`
--- api/tools/config.ts
+++ api/tools/config.ts
@@ -6,8 +6,8 @@ export const POSTGRES_URI =
     env.POSTGRES_URI || 'postgresql://localhost:5432/velocitymanager';
 export const RABBIT_MQ_URI = env.RABBIT_MQ_URL || 'amqp://localhost';
 
-export const RABBIT_MQ_PREFIX =
-    env.RABBIT_MQ_PREFIX || 'eps-dev.velocity-manager';
+// Sync with MC servers bugged for some reason
+export const RABBIT_MQ_PREFIX = env.RABBIT_MQ_PREFIX || '';
 
 export const CONTACT_TIMEOUT_SECONDS = BigInt(
     Number(env.CONTACT_TIMEOUT_SECONDS || '10') * 1000
