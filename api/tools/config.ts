5b12a75baf79fe9c7522a93e60e170c112d792ce
Elias Schramm <privat@eps-dev.de>
2022-09-24 19:45:01 +0200

    dev: Fix Compile Times With WSL

--- api/tools/config.ts
+++ api/tools/config.ts
@@ -1,6 +1,6 @@
 import { env } from 'process';
 
-export const BIND_PORT = Number(env.BIND_PORT || '80');
+export const BIND_PORT = Number(env.BIND_PORT || '30001');
 
 export const POSTGRES_URI =
     env.POSTGRES_URI || 'postgresql://localhost:5432/velocitymanager';
